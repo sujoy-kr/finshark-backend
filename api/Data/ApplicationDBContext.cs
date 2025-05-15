0e322da1ffab0006be92c784c7416ee0f9db13b9
Sujoy Karmakar <sujoykr8@gmail.com>
2025-05-15 21:17:55 +0600

    Register

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,25 @@ namespace api.Data
         { }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole {
+                    Id = "1",
+                    Name ="Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole {
+                    Id = "2",
+                    Name ="User",
+                    NormalizedName = "USER"
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
